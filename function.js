fd4765aa7c4c2232fa6163534839116d7df80578
Justin Moreira <justinmoreira23@gmail.com>
2023-07-27 13:13:03 -0400

    Create checkWinner function

--- function.js
+++ function.js
@@ -1,12 +1,30 @@
 function getComputerChoice(){
     let selection = Math.floor(Math.random() * 3);
     if(selection == 0){
-        return "rock";
+        return "Rock";
     } else if (selection == 1){
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
-
-console.log(getComputerChoice());
+function checkWinner(playerSelection, computerSelection){
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+    if(computerSelection == "Rock" && playerSelection == "Scissors"
+        || computerSelection == "Paper" && playerSelection == "Rock"
+        || computerSelection == "Scissors" && playerSelection == "Paper"){
+            return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else if(playerSelection == "Rock" && computerSelection == "Scissors"
+    || playerSelection == "Paper" && computerSelection == "Rock"
+    || playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if(playerSelection == computerSelection){
+        return "It's a tie!, you both picked " + playerSelection;
+    }
+    else{
+        return "Invalid input";
+    }
+}
+console.log(checkWinner("paper", getComputerChoice()));
